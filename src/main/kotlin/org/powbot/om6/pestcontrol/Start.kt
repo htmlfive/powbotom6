e049cd4aa1cf0a1f1490ff829223e27bda3468db
htmlfive <shawn.khaan@gmail.com>
2025-12-02 14:42:28 +0000

    Delete src/main/kotlin/org/powbot/om6/pestcontrol directory

--- src/main/kotlin/org/powbot/om6/pestcontrol/Start.kt
+++
@@ -1,9 +0,0 @@
-package org.powbot.om6.pestcontrol
-
-/**
- * Entry point for standalone execution.
- */
-fun main() {
-    val script = PestControl()
-    script.startScript("localhost", "WebWalker", false)
-}
